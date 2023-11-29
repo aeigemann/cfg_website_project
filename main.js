49c8bb79ebcae774dfd09f397577005d2b6e1825
Annika Eigemann <annika.eigemann@studium.fernuni-hagen.de>
2023-11-29 09:45:46 +0000

    fixed image size, moved JS functionality to seperate js file

---
+++ main.js
@@ -0,0 +1,8 @@
+// open and close sidemenu on small screens
+function openmenu(){
+    document.getElementById("sidemenu").style.right = "0";
+}
+
+function closemenu(){
+    document.getElementById("sidemenu").style.right = "-200px";
+}
